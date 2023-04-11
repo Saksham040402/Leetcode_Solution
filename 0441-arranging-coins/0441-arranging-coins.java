d04adc6d614768680d917da95a0a9da9ab8834a6
Saksham Gangwar <82796850+Saksham040402@users.noreply.github.com>
2023-04-11 09:48:50 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (72.95%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,19 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int s = 0;
-        int a = 2;
-        if(n>0)
-            s+=1;
-        n = n-1;
-        
-        while(n > 0)
-        {
-            n = n-a;
-            if(n>=0)
-                s+=1;
-            
-            a++;
-        }
-        return s;
+        return (int)(Math.sqrt(2 * (long)n + 0.25) - 0.5);
     }
 }
