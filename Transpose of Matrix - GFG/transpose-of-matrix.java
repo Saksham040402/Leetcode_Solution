dd0d4480ad4c913004e0387b22cea7c9aac8afa4
Saksham Gangwar <82796850+Saksham040402@users.noreply.github.com>
2023-07-10 12:20:58 +0530

    Added solution - LeetHub

--- Transpose of Matrix - GFG/transpose-of-matrix.java
+++ Transpose of Matrix - GFG/transpose-of-matrix.java
@@ -55,11 +55,5 @@ class Solution
                 r++;
             }
         }
-        // for(int i = 0; i < n;i++){
-        //     for(int j = 0; j< n;j++){
-        //         System.out.print(ar[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
     }
 }
