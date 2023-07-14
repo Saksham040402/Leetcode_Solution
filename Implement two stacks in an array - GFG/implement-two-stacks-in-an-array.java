2b21672d5c4d93ca91ab4bf0d873217e111081b7
Saksham Gangwar <82796850+Saksham040402@users.noreply.github.com>
2023-07-14 12:52:03 +0530

    Added solution - LeetHub

--- Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.java
+++ Implement two stacks in an array - GFG/implement-two-stacks-in-an-array.java
@@ -75,11 +75,9 @@ class twoStacks
         if(top1==-1) return -1;
         return arr[top1--];
     }
-  
     int pop2()
     {
         if(top2==size) return -1;
         return arr[top2++];
     }
 }
-
