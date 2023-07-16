a237aea1d4604c583ecb794744962e07219bdb10
Saksham Gangwar <82796850+Saksham040402@users.noreply.github.com>
2023-07-16 21:01:39 +0530

    Added solution - LeetHub

--- Queue Reversal - GFG/queue-reversal.java
+++ Queue Reversal - GFG/queue-reversal.java
@@ -53,7 +53,7 @@ class GfG{
     //Function to reverse the queue.
     public Queue<Integer> rev(Queue<Integer> q){
         //add code here.
-        Stack<Integer> s =  new Stack<>();
+         Stack<Integer> s =  new Stack<>();
         while(!q.isEmpty())
         s.push(q.remove());
         
